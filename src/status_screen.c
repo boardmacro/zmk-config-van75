23dff6841d311dfd38efcde0e5317a4efb456340
boardmacro <bengkellgue@gmail.com>
2025-09-08 02:59:50 +0700

    Delete src directory

--- src/status_screen.c
+++
@@ -1,9 +0,0 @@
-#include <lvgl.h>
-#include <zmk/display/widgets/active_key.h>
-
-static lv_obj_t *active_key_widget;
-
-void zmk_display_init_widgets(lv_obj_t *parent) {
-    active_key_widget = zmk_widget_active_key_create(parent);
-    lv_obj_align(active_key_widget, LV_ALIGN_CENTER, 0, 0);
-}
