1bb7609ffa47a0b2c2b21f5ac16a58f570435e9c
boardmacro <bengkellgue@gmail.com>
2025-09-08 03:45:09 +0700

    Delete config/src directory

--- config/src/status_screen.c
+++
@@ -1,7 +0,0 @@
-#include <lvgl.h>
-
-void zmk_display_init_widgets(lv_obj_t *parent) {
-    lv_obj_t *label = lv_label_create(parent);
-    lv_label_set_text(label, "OLED TEST");
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
-}
